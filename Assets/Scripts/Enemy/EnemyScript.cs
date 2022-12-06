287e741af214359b3bc17bfada498c984b6f5e09
Stacey <stacey.munoz@ymail.com>
2022-12-06 00:56:11 -0800

    Finished player and enemy animation
    Enemy animation works when they have a target to go for they walk, attack, and die. Player animation is a but buggy but it mostly follows the mouse position I have the Player death animation ready but we haven't added the code for player death.

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -8,6 +8,8 @@ public class EnemyScript : MonoBehaviour
     public Rigidbody2D bod;
     public Collider2D col;
 
+    private Animator anim;
+
     //Movement
     [Space]
     public GameObject target;
@@ -26,13 +28,16 @@ public class EnemyScript : MonoBehaviour
         //target = FindObjectOfType<PlayerController>();
 
         health = maxHP;
+
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         if(health <= 0)
         {
-            Destroy(gameObject);
+            anim.SetBool("IsHurt", true);
+            //Destroy(gameObject);
         }
     }
 
@@ -81,5 +86,22 @@ public class EnemyScript : MonoBehaviour
         }
 
         gameObject.transform.position = new Vector3(transform.position.x + speedX * Time.deltaTime, transform.position.y + speedY * Time.deltaTime, transform.position.z);
+
+        if (diff.x != 0 || diff.y != 0)
+        {
+            anim.SetFloat("X", diff.x);
+            anim.SetFloat("Y", diff.y);
+            anim.SetBool("IsWalking", true);
+        } else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+        if (diff.x <= 0.001 || diff.y <= 0.001)
+        {
+            anim.SetBool("IsAttacking", true);
+        } else
+        {
+            anim.SetBool("IsAttacking", false);
+        }
     }
 }
